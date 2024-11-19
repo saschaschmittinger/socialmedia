021779d02e72f1eda5038c9a8f3d9b86396b9a01
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-19 10:38:34 +0100

    19_11-24

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, signup
 
 app_name = "core"
 
-urlpatterns = [path("", home, name="home_view")]
+urlpatterns = [
+    path("", home, name="home_view"),
+    path("signup/", signup, name="signup_view"),
+]
--- core/views.py
+++ core/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "index.html")
+    title = "SSC SocialMedia"
+    context = {"title": title}
+    return render(request, "index.html", context)
+
+
+def signup(request):
+    title = "SSC Sign Up"
+    context = {"title": title}
+    return render(request, "signup.html", context)
