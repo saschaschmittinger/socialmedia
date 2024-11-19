30a8599429a3ba34e194381ec04466f541ec2e09
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-19 11:21:29 +0100

    19_11_24

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 def home(request):
@@ -9,5 +11,26 @@ def home(request):
 
 def signup(request):
     title = "SSC Sign Up"
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "E-Mail Adresse existiert bereits")
+                return redirect("core:signup_view")
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Benutzername existiert bereits")
+                return redirect("core:signup_view")
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password
+                )
+                user.save()
+                return redirect("core:signup_view")
+
     context = {"title": title}
     return render(request, "signup.html", context)
