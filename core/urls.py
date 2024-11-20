79754f5157470ac85bffa92db32e420566fdb8e7
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-20 10:06:52 +0100

    20_11_24

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, signup
+from .views import home, signup, login_view
 
 app_name = "core"
 
 urlpatterns = [
     path("", home, name="home_view"),
     path("signup/", signup, name="signup_view"),
+    path("login/", login_view, name="login_view"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from .models import Profile
 from django.contrib import messages
 
 
@@ -11,7 +12,8 @@ def home(request):
 
 
 def signup(request):
-    title = "SSC Sign Up"
+    title: str = "SSC Sign Up"
+    template: str = "signup.html"
 
     if request.method == "POST":
         username = request.POST["username"]
@@ -19,7 +21,7 @@ def signup(request):
         password = request.POST["password"]
         password2 = request.POST["password2"]
 
-        if password == password2:
+        if password == password2 and not password == "" and not password2 == "":
             if User.objects.filter(email=email).exists():
                 messages.info(request, "E-Mail Adresse existiert bereits")
                 return redirect("core:signup_view")
@@ -32,10 +34,27 @@ def signup(request):
                 )
                 user.save()
                 messages.success(request, "Account wurde erfolgreich angelegt")
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id
+                )
+                new_profile.save()
                 return redirect("core:signup_view")
+
+        elif password == "" and password2 == "":
+            messages.error(request, "Bitte alle Felder ausfüllen!")
+
         else:
             messages.error(request, "Passwörter stimmen nicht überein")
             return redirect("core:signup_view")
 
-    context = {"title": title}
-    return render(request, "signup.html", context)
+    context: dict = {"title": title}
+    return render(request, template, context)
+
+
+def login_view(request):
+    title: str = "SSC Login"
+    template: str = "signin.html"
+    context: dict = {"title": title}
+    return render(request, template, context)
